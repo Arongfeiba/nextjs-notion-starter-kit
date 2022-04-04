35c2a725205a48af3a21e1d1ebedec705906eacd
Arongfeiba <102794250+Arongfeiba@users.noreply.github.com>
2022-04-04 20:59:41 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://space.bilibili.com/383352624'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
